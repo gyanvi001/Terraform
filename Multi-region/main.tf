ec48d538c1b91000b678848716bc4f84ca10703c
Gyanvi Pandey <155666897+gyanvi001@users.noreply.github.com>
2025-04-29 10:57:46 +0000

    Initial

--- Multi-region/main.tf
+++ Multi-region/main.tf
@@ -31,3 +31,21 @@ module "aws_vpc" {
     }
 }
 
+
+# -------------------------
+# Define Required Providers
+# -------------------------
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.3"
+}
