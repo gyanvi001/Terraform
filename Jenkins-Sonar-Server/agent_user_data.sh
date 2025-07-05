a572923ac055f33ae74299b7971dd0cb72f0f8c2
gyanvi001 <gyanvipandey01@gmail.com>
2025-07-05 11:25:14 +0530

    Modify

--- Jenkins-Sonar-Server/agent_user_data.sh
+++ Jenkins-Sonar-Server/agent_user_data.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+#Update the pacjage list 
+sudo apt update -y
+sudo apt install -y git curl wget vim
+sudo apt upgrade -y
+
+#Install Java (required by jenkins )
+sudo apt install -y openjdk-17-jdk
