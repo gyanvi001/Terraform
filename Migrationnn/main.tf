0ed7e0f3a9f1cd42d89fc820960bc9d8a7c1f961
gyanvi001 <gyanvipandey01@gmail.com>
2025-07-06 21:47:57 +0530

    Modify

--- Migrationnn/main.tf
+++ Migrationnn/main.tf
@@ -6,3 +6,24 @@ provider "aws" {
 #  to = aws_instance.example
  # id = "i-0f81f011c1ea33ea9"
 #}
+
+resource "aws_s3_bucket" "backend-bucket" {
+  bucket = "my-terraform-backend-bucket"
+  tags = {
+    Name        = "My Terraform Backend Bucket"
+    Environment = "Dev"
+  }
+}
+ 
+resource "aws_s3_bucket_versioning" "bucket-versioning" {
+  bucket = aws_S3_bucket.backend-bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+  
+  lifecycle {
+    prevent_destroy = false
+  }
+
+
+}
