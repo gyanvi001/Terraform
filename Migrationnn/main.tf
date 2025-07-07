c6bfc98e93892687580b03c7a318f0656eb4f5eb
gyanvi001 <gyanvipandey01@gmail.com>
2025-07-07 22:31:46 +0530

    Update

--- Migrationnn/main.tf
+++ Migrationnn/main.tf
@@ -2,35 +2,28 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-#import {
-#  to = aws_instance.example
- # id = "i-0f81f011c1ea33ea9"
-#}
-
 resource "aws_s3_bucket" "new-backend-bucket" {
-  bucket = "new-my-terraform-backend-bucket" # This is the bucket name through which it will be accessed
+  bucket = "new-my-terraform-backend-bucket"
   tags = {
     Name        = "My Terraform Backend Bucket"
     Environment = "Dev"
   }
 }
- 
+
 resource "aws_s3_bucket_versioning" "bucket-versioning" {
   bucket = aws_s3_bucket.new-backend-bucket.id
   versioning_configuration {
     status = "Enabled"
   }
-  
   lifecycle {
     prevent_destroy = false
   }
-
 }
 
 resource "aws_dynamodb_table" "terraform_state_dynamo" {
-  name = "my-table"
+  name         = "my-table"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "LockID"
+  hash_key     = "LockID"
 
   attribute {
     name = "LockID"
@@ -38,13 +31,13 @@ resource "aws_dynamodb_table" "terraform_state_dynamo" {
   }
 }
 
-#provisioner
-resource "aws_ec2" "My-instance" {
+resource "aws_instance" "My-instance" {
   ami           = "ami-088231783271"
   instance_type = "t2.micro"
-  key_name     =  "my-key-pair"
+  key_name      = "my-key-pair"
+
   root_block_device {
-    volume_size = 8 
+    volume_size = 8
     volume_type = "gp3"
   }
 
@@ -53,15 +46,15 @@ resource "aws_ec2" "My-instance" {
     Environment = "Dev"
   }
 
-}
+  provisioner "file" {
+    source      = "local_file.txt"
+    destination = "/home/ubuntu/local_file.txt"
 
-# file provisioner 
-provisioner "file" {
-  source     = "local_file.txt"
-  destination = "/home/ubuntu/local_file.txt"
-  connection {
-    type        = "ssh"
-    user   = "ubuntu"
-    key_name = "my-key-pair"
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/my-key-pair.pem")  # Corrected here
+      host        = self.public_ip
     }
+  }
 }
